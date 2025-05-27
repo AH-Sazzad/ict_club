e7183ab5b00fba64da0eaf8e57d2143220846a74
AH-Sazzad <mdasaduzzamanasad100@gmail.com>
2025-05-27 22:22:00 +0600

    update db

--- blog_pro.sql
+++ blog_pro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2025 at 04:30 PM
+-- Generation Time: May 27, 2025 at 06:20 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -180,7 +180,6 @@ CREATE TABLE `comment` (
 --
 
 INSERT INTO `comment` (`id`, `name`, `email`, `comment`) VALUES
-(1, 'AH_Sazzad', 'mdasaduzzamanasad100@gmail.com', 'Demo'),
 (2, 'AH_Sazzad', 'mdasaduzzamanasad100@gmail.com', 'Demo'),
 (3, 'AH_Sazzad', 'mdasaduzzamanasad100@gmail.com', 'Demo'),
 (4, 'AH_Sazzad', 'mdasaduzzamanasad100@gmail.com', 'Demo'),
